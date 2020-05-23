006b3cb4db3dd95e9862e8c998ccbcef2ea59e3b
AtulRaj151 <37062660+AtulRaj151@users.noreply.github.com>
2020-05-24 00:23:59 +0530

    add mongodb

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port  = 8000;
 const expressLayout = require('express-ejs-layouts');
+
+const db = require('./config/mongoose');
 app.use(express.static('./assets'));
 
 app.use(expressLayout);
 
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
+
 
 
 //use express router
