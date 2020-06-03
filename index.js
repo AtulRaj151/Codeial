79be3246f15c7e29debcd77fa7086224a2273549
AtulRaj151 <37062660+AtulRaj151@users.noreply.github.com>
2020-06-04 01:37:01 +0530

    add post schema

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ const db = require('./config/mongoose');
 
 const MongoStore = require('connect-mongo')(session);
 
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+     src:'./assets/scss',
+     dest:'./assets/css',
+     debug:true,
+     outputStyle:'extended',
+     prefix:'/css'
+}
+
+));
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 
