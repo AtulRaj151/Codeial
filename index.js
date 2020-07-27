356f3c4c6da382c5d66a645c6888459b552de48f
unknown <atulsid151@gmail.com>
2020-07-28 00:31:24 +0530

    add chatting Engine socket.io

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const passport = require('passport');
 const googlePassport = require('./config/passport-google-oauth2-strategy');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+// sety the chat serer to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("Server is Listening on port 5000");
+
+
+
+
 
 
 const db = require('./config/mongoose');
