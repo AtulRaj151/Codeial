91d04e1c7a90382058ecd2f3596c7a8888158d68
unknown <atulsid151@gmail.com>
2020-08-01 19:58:04 +0530

    add socket address for production

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine {
             this.chatBox = $(`#${chatBoxId}`);
             this.userEmail = userEmail;
 
-            this.socket = io.connect('http://localhost:5000');
+            this.socket = io.connect('http://54.209.163.42:5000');
 
             if(this.userEmail){
                 this.connectionHandler();
